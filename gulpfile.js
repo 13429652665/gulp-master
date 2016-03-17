ab9111b9e60e0fa9c1cedc4dd9cae14dfd2d4643
13429652665 <455827678@qq.com>
2016-03-17 09:29:53 +0800

    提交测试

--- gulpfile.js
+++ gulpfile.js
@@ -69,7 +69,7 @@ gulp.task('css', function() {
         .pipe(cssmin({
             advanced: false,//类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
             compatibility: 'ie7',//保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
-            keepBreaks: true//类型：Boolean 默认：false [是否保留换行]
+            keepBreaks: false//类型：Boolean 默认：false [是否保留换行]
         }))
         .pipe(sourcemaps.write('maps', {addComment: false}))
         .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
